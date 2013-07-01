f33a87f2489a674752db488be6665eeb415e18bd
Jonathan Zrake <jonathan.zrake@gmail.com>
2013-06-30 20:04:12 -0400

    Add a save/load function to the ztree-test file

--- ztree-test.c
+++ ztree-test.c
@@ -3,11 +3,14 @@
 #include "ztree.h"
 #include "zmesh.h"
 
+#define TREE_FILE_NAME "data/tree-1d.dat"
 #define nohup 0
 #define asserteq(E, v)printf("%s == %d : %d\n",#E,v,E); assert(E==v||nohup);
 
-//static void output_tree(const struct ztree *T, const char *fname);
-//static void output_faces(const struct zmesh *M, const char *fname);
+void output_tree(const struct ztree *T, const char *fname);
+void output_faces(const struct zmesh *M, const char *fname);
+void save_tree(const struct ztree *T, const char *fname);
+void load_tree(struct ztree *T, const char *fname);
 
 int test_tree()
 {
@@ -53,6 +56,24 @@ int test_tree()
   return 0;
 }
 
+int test_read()
+{
+  struct ztree *tree0 = ztree_new(1, 0);
+  struct ztree *tree1 = ztree_new(1, 0);
+  int n;
+
+  for (n=0; n < 1<<10; ++n) if (n % 131 == 0) ztree_require_node(tree0, 10, &n);
+  for (n=0; n < 1<<8 ; ++n) if (n %  83 == 0) ztree_require_node(tree0,  8, &n);
+  for (n=0; n < 1<<6 ; ++n) if (n %  13 == 0) ztree_require_node(tree0,  6, &n);
+  for (n=0; n < 1<<4 ; ++n) if (n %   7 == 0) ztree_require_node(tree0,  4, &n);
+
+  save_tree(tree0, "tree.dat");
+  load_tree(tree1, "tree.dat");
+
+  asserteq(ztree_count(tree0, ZTREE_NODE), ztree_count(tree1, ZTREE_NODE));
+  return 0;
+}
+
 int test_mesh()
 {
   return 0;
@@ -62,6 +83,7 @@ int main(int argc, char **argv)
 {
   test_tree();
   test_mesh();
+  test_read();
   return 0;
 }
 
@@ -92,4 +114,25 @@ void output_faces(const struct zmesh *M, const char *fname)
     fprintf(outf, "%f %f\n", 0.5 * (xL + xR), z);
   }
   fclose(outf);
+}
+
+void save_tree(const struct ztree *T, const char *fname)
+{
+  FILE *outf = fopen(fname, "w");
+  struct ztree *it = NULL;
+  while ((it = ztree_next_leaf(T, it))) {
+    fprintf(outf, "%d %d\n", ztree_depth(it), ztree_index(it, 0));
+  }
+  fclose(outf);
+}
+
+void load_tree(struct ztree *T, const char *fname)
+{
+  FILE *inf = fopen(fname, "r");
+  int depth, I;
+  while (!feof(inf)) {
+    fscanf(inf, "%d %d\n", &depth, &I);
+    ztree_require_node(T, depth, &I);
+  }
+  fclose(inf);
 }
